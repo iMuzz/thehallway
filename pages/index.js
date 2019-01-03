ead73d15dd9770e917cce7738e025392f54fa884
Faraaz Nishtar <faraaznishtar@gmail.com>
2019-01-02 20:15:56 -0800

    Inital commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Box from 'ui-box'
+
+export default () => {
+  return(
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <img src="https://file-shekcetdig.now.sh/" />
+    </Box>
+  )
+}
